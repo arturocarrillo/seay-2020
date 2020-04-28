151a8a98b142377939d15439a1ce6a5810225a73
hector arturo pool carrillo <46902351+arturocarrillo@users.noreply.github.com>
2020-04-28 09:25:08 -0500

    Add files via upload

---
+++ news/news-firm-conv-2020.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="es-MX" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+<head>
+  <!-- essential -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="language" content="spanish">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
+  <!-- essential -->
+  <link rel="canonical" href="http://www.seay.org.mx/news/news-firm-conv-2019.php">
+  <link rel="shortlink" href="http://seay.org.mx/news/news-firm-conv-2019.php">
+  <meta name="description" content="Secretaría, Ejecutiva,Sistema, Estatal, Anticorrupción, Yucatán, SESEAY">
+  <meta name="keywords" content="Secretaría, Ejecutiva,Sistema, Estatal, Anticorrupción, Yucatán, SESEAY">
+  <meta name="author" content="Secretaría Ejecutiva">
+  <!-- verified your ownership with google -->
+  <meta name="google-site-verification" content="GlbZMatmm_8TK0bVts2nVIhDcP-gPbnH5xluJN47RzM">
+  <title>Firma de convenio</title>
+  <!-- Open Graph -->
+  <meta property="og:url" content="http://www.seay.org.mx/news/news-firm-conv-2019.php">
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="Secretaría Ejecutiva del Sistema Estatal Anticorrupción de Yucatán">
+  <meta property="og:description" content="Secretaría, Ejecutiva,Sistema, Estatal, Anticorrupción, Yucatán, SESEAY">
+  <meta property="og:image" content="https://indra.hosting-mexico.net:2083/cpsess7660515856/frontend/paper_lantern/filemanager/showfile.html?file=logo-header-3.png&fileop=&dir=%2Fhome%2Fseayorgm%2Fpublic_html%2Fimg&dirop=&charset=&file_charset=&baseurl=&basedir=">
+  <!-- icon page & mobile apple -->
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <link rel="apple-touch-icon" sizes="57x57" href="http://www.seay.org.mx/img/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="http://www.seay.org.mx/img/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="http://www.seay.org.mx/img/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="http://www.seay.org.mx/img/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="http://www.seay.org.mx/img/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="http://www.seay.org.mx/img/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="http://www.seay.org.mx/img/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="http://www.seay.org.mx/img/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="http://www.seay.org.mx/img/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="http://www.seay.org.mx/img/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="http://www.seay.org.mx/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="http://www.seay.org.mx/img/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="http://www.seay.org.mx/img/favicon-16x16.png">
+  <link rel="manifest" href="http://www.seay.org.mx/img/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="http://www.seay.org.mx/img/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <!-- fonts & icons -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="http://www.seay.org.mx/css/style_icon.css">
+  <!-- styles site -->
+  <link rel="stylesheet" type="text/css" href="http://www.seay.org.mx/css/scroll-top.css">
+  <link rel="stylesheet" type="text/css" href="http://www.seay.org.mx/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="http://www.seay.org.mx/css/style-footer.css">
+  <link rel="stylesheet" href="http://www.seay.org.mx/css/baguetteBox.min.css">
+  <link rel="stylesheet" href="http://www.seay.org.mx/css/compact-gallery.css">
+  <script src="http://www.seay.org.mx/js/jquery-3.1.1.min.js" type="text/javascript" charset="utf-8"></script> 
+  <script src="http://www.seay.org.mx/js/script4.js" type="text/javascript" charset="utf-8"></script> 
+  <script src="http://www.seay.org.mx/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
+  <script>
+    $(document).ready(function () {
+        $('.up').click(function(){
+            $('body,html').animate({
+                scrollTop: '0px'
+            }, 1000);
+        });
+        $(window).scroll(function(){
+            if( $(this).scrollTop() > 0 ){
+                $('.up').slideDown(600);
+            }else {
+                $('.up').slideUp(600);
+            }
+        });
+        /*hacia abajo*/
+        $('.ir-abajo').click(function(){
+            $('body,html').animate({
+                  scrollTop: '1000px'
+            }, 1000);
+        });
+    });
+  </script>
+</head>
+<body>
+	<div class="up">
+    	<span class="ir-arriba icon-arrow-up2">
+    	</span>
+	</div>
+	<?php include'../header.php';?>
+    <section class="w-100 flex justify-content-center mt-10 mb-14">
+    <div class="w-80 mt-5 mb-4 flex">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="http://www.seay.org.mx/releases-2020.php">Comunicados 2020</a></li>
+              <li class="breadcrumb-item"><a href="http://www.seay.org.mx/releases.php">Comunicados 2019</a></li>
+            </ol>
+          </nav>
+    </div>
+    <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-xs-12 flex">
+      <div class="w-100 mt-5 mb-5 flex">
+        <h2 class="title text-blue_1">Convenios de colaboración 
+        </h2>
+      </div>
+      <div class="w-100 mt-5 mb-5 flex">
+        <p class="paragraph-md text-blue_1 p-2">
+              El Sistema Estatal Anticorrupción (SEAY), en colaboración con la Secretaría Ejecutiva del Sistema Estatal Anticorrupción de Yucatán (SESEAY), en un esfuerzo conjunto por llevar acciones que incentiven la participación ciudadana, la inserción del sector empresarial y educativo en nuestra entidad en pro de la lucha contra la corrupción, han llevado a cabo diversas actividades, entre las cuales se resaltan múltiples visitas a instancias que han servido de enlace de la SESEAY con el objetivo de entablar canales de comunicación, trabajo en equipo con la ciudadanía y firmas de convenios para trabajar de la mano en proyectos anticorrupción y cultura de integridad. 
+        </p>
+        
+        <p class="paragraph-md text-blue_1 p-2 bold">
+            Hasta la fecha se han firmado 23 convenios con las siguientes instancias: 
+        </p>
+        <table class="w-90 m-auto mt-5 mb-5 table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Nombre de la instancia</th>
+                <th scope="col">Tipo de instancia</th>
+                <th scope="col">Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Instituto Tecnológico Superior del Sur (Oxkutzcab) - ITSS
+                </th>
+                <td>Institución Educativa</td>
+                <td>10 de enero 2020</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Mesoamericana de San Agustín - UMSA
+                </th>
+                <td>Institución Educativa</td>
+                <td>11 de noviembre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Privada de la Península - UPP
+                </th>
+                <td>Institución Educativa</td>
+                <td>06 de noviembre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Interamericana para el Desarrollo – UNID
+                </th>
+                <td>Institución Educativa</td>
+                <td>25 de octubre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Tecnológica del Centro (Izamal) – UTC
+                </th>
+                <td>Institución Educativa</td>
+                <td>21 de octubre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad de Oriente (Valladolid) - UNO
+                </th>
+                <td>Institución Educativa</td>
+                <td>16 de octubre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Instituto Tecnológico Superior Progreso – ITS Progreso
+                </th>
+                <td>Institución Educativa</td>
+                <td>9 de octubre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Asociación Nacional de Abogados AC – ANADE 
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>19 de septiembre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Tecnológica Metropolitana – UTM 
+                </th>
+                <td>Institución Educativa</td>
+                <td>18 de septiembre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Escuela Superior de Artes de Yucatán - ESAY
+                </th>
+                <td>Institución Educativa</td>
+                <td>9 de septiembre 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Tecnológica de Tizimín 
+                </th>
+                <td>Institución Educativa</td>
+                <td>19 de agosto 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Vizcaya de las Américas. Campus Mérida
+                </th>
+                <td>Institución Educativa</td>
+                <td>15 de agosto 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Consejo de Notarios de Yucatán
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>4 de julio 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Comisión de Derechos Humanos del Estado de Yucatán – CODHEY
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>1 de julio 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Unidad de Contraloría Municipal - Municipio de Mérida.
+                </th>
+                <td>Organización gubernamental</td>
+                <td>3 de julio 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Asociación de Scouts México, A.C.
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>5 de abril 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Centro Empresarial de Mérida – COPARMEX 
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>21 de enero 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Cámara Nacional de Comercio, Servicios y Turismo de Mérida – CANACO
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>21 de enero 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Universidad Autónoma de Yucatán – UADY
+                </th>
+                <td>Institución Educativa</td>
+                <td>21 de enero 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Cámara Mexicana de la Industria de la Construcción – CMIC
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>21 de enero 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Asociación Mexicana de la Industria de la Construcción – AMIC
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>21 de enero 2019</td>
+              </tr>
+              <tr>
+                <th scope="row">Asociación Mexicana de Mujeres Empresarias de Yucatán – AMMEYUC
+                </th>
+                <td>Organización no gubernamental</td>
+                <td>21 de enero 2019</td>
+              </tr>
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+	<?php include'../footer.php';?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script>
+        baguetteBox.run('.compact-gallery', { animation: 'slideIn'});
+    </script>
+</body>
+</html>
